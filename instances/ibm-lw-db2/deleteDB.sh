fd1fac78fd0390c9bfc4ab244c9e7e12f4b0023a
Shady Attia <Shady.Attia@ibm.com>
2023-06-27 13:17:08 -0400

    add user

--- instances/ibm-lw-db2/deleteDB.sh
+++ instances/ibm-lw-db2/deleteDB.sh
@@ -6,6 +6,7 @@ source /home/db2inst1/sqllib/db2profile
 # Define the connection string
 CONNECTION_STRING="DATABASE=$DB_NAME;HOSTNAME=$DB_HOST;PORT=$DB_PORT;PROTOCOL=TCPIP;UID=$DB_USER;PWD=$DB_PASS;"
 
+su db2inst1
 # Connect to the DB2 database
 db2 connect to $CONNECTION_STRING
 
