9e0b1449bd5ada0994193b22e6b01d70e856785b
Shady Attia <Shady.Attia@ibm.com>
2023-06-26 21:16:02 -0400

    drop db

---
+++ instances/ibm-lw-db2/deleteDB.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Source the db2profile
+source /home/db2inst1/sqllib/db2profile
+
+# Define the connection string
+CONNECTION_STRING="DATABASE=$DB_NAME;HOSTNAME=$DB_HOST;PORT=$DB_PORT;PROTOCOL=TCPIP;UID=$DB_USER;PWD=$DB_PASS;"
+
+# Connect to the DB2 database
+db2 connect to $CONNECTION_STRING
+
+# Drop the DB2 database
+db2 drop database $DB_NAME
